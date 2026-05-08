f0016aa37463e302afeb56cf25651d95456432a3
himanshudubey <hackerhimanshudubey7778@gmail.com>
2026-05-08 22:12:16 +0530

    update and new commit add

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from './Card';
 import './Expenses.css';
@@ -5,6 +6,18 @@ import './Expenses.css';
 function Expenses(props) {
   return (
     <Card className="expenses">
+      {/* Aaj ka naya feature: Filter Dropdown (Abhi sirf UI hai) */}
+      <div className='expenses-filter'>
+        <div className='expenses-filter__control'>
+          <label style={{color: 'white'}}>Filter by year</label>
+          <select>
+            <option value='2026'>2026</option>
+            <option value='2025'>2025</option>
+            <option value='2024'>2024</option>
+          </select>
+        </div>
+      </div>
+
       {props.items.map((expense) => (
         <ExpenseItem
           key={expense.id}
